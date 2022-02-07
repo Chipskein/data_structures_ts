dfd9291d46bb32fba67f5c4d4499e75940ef12b3
Chipskein <abfn0905@gmail.com>
2022-02-07 18:39:12 -0300

    feat:add elements to bst

--- structures/binary_search_tree.ts
+++ structures/binary_search_tree.ts
@@ -5,6 +5,7 @@ const warn=colors.yellowBright;
 const error=colors.red;
 class node<T>{
     public data:number;
+    public parent:node<T>| null=null;
     public left:node<T> | null=null;
     public right:node<T> | null=null;
     constructor(data:number){
@@ -13,58 +14,50 @@ class node<T>{
 }
 class BinarySearchTree<T>{
     private size=0
-    private root:node<T> | null=null;
+    private root:any;
     public isEmpty(){return this.size==0}
     public addElement(data:number){
-        this.root=this.recursive_Add(this.root,data)
-        this.size++;
-    }
-    private recursive_Add(newnode:any,data:number){
-        if(newnode==null) newnode=new node(data);
+        if(!this.isEmpty()){
+            let isNodeAdded=this.add_recursive(data,this.root)
+            this.size++
+        }
         else{
-            if(newnode.data>data) newnode.left=this.recursive_Add(newnode.left,data)
-            if(newnode.data<data) newnode.right=this.recursive_Add(newnode.right,data) 
+            this.root=new node(data)
+            this.root.parent=this.root;
+            this.size++
         }
-        return newnode
     }
-    public removeElement(data:number,root = this.root,parent = this.root){
- 	 if(root!=null&&parent!=null){
-	 	if(data==root.data){
-			console.log("Found Node to Delete");
-			//if is a leef node;
-			if(root.left==null&&root.right==null){
-				if(parent?.left?.data==root.data){
-					parent.left=null;
-				}
-				else{
-				  	parent.right=null;
-				}
-				console.log("Removed",data);
-			}
-		}
-
-		else{
-            if(root.left==null&&root.right==null){
-                console.log("Didn't found node do delete");
-                return -1;
-            }
-			if(data<root.data) this.removeElement(data,root.left,root);
-			if(data>root.data) this.removeElement(data,root.right,root);	
-		}
-	 }
+    public add_recursive(data:number,parent:any):any{
+        let child=new node(data);
+        child.parent=parent;
+        
+        if(parent.data>child.data&&parent.left==null){
+            parent.left=child
+            return 1
+        }
+        if(parent.data<child.data&&parent.right==null){
+            parent.right=child
+            return 1
+        }
+        if(parent.data>data) return this.add_recursive(data,parent.left);
+        if(parent.data<data) return this.add_recursive(data,parent.right);
     }
-    public find(data:number,root = this.root){
+    public removeElement(data:number,root = this.root){
+        let nodeExists=this.find(data,root);
+        console.log(nodeExists)
+        if(nodeExists!=-1) console.log("node found")
+        else return -1 
+    }
+    public find(data:number,root = this.root):any{
         if(root!=null){
-            if(data==root.data) console.log("FOUND",data)
+            if(data==root.data) return root
             else {
-                if(root.left==null&&root.right==null) console.log("DIDN'T FIND",data);
-                if(data<root.data) this.find(data,root.left);
-                if(data>root.data) this.find(data,root.right); 
-               
+                if(root.left==null&&root.right==null) return -1
+                if(data<root.data) return this.find(data,root.left);
+                if(data>root.data) return this.find(data,root.right); 
             }
         }
     }
-    public replace(data:number,newdata:number){}
 }
 export function TestBinarySearchTree(){
     console.clear();
@@ -74,14 +67,21 @@ export function TestBinarySearchTree(){
     console.log(pass("Creating Binary Search Tree[OK]"));
     console.log(warn("---------------------------------------------------"))
     console.log(doing("Adding elements to BST..."));
-        let elements=[74,26,100,20,50,90,200];
-        elements.forEach(element => {
-            bst.addElement(element);
-        });
+        let cached_numbers=[];
+        for(let c=10;c>0;c--){
+            let number=Math.round(Math.random()*300)
+            cached_numbers.push(number);
+            bst.addElement(number);    
+        }
     console.log(pass("Adding elements to BST[OK]"));
     console.log(warn("---------------------------------------------------"))
-    console.log(bst);
-    bst.removeElement(20);
+    console.log(cached_numbers)
+    console.log(warn("PRINT ELEMENTS FROM BST..."));
+        cached_numbers.forEach(number => {
+            console.log(bst.find(number));
+        });
+    console.log(pass("PRINT ELEMENTS FROM BST[OK]"));
+
 }
 TestBinarySearchTree()
 
