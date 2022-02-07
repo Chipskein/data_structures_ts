221faf77a5b187e2627ead4c1b3e9d1aa7bf6757
Chipskein <abfn0905@gmail.com>
2022-02-07 20:43:33 -0300

    feat:binary search tree remove

--- structures/binary_search_tree.ts
+++ structures/binary_search_tree.ts
@@ -27,7 +27,7 @@ class BinarySearchTree<T>{
             this.size++
         }
     }
-    public add_recursive(data:number,parent:any):any{
+    private add_recursive(data:number,parent:any):any{
         let child=new node(data);
         child.parent=parent;
         
@@ -42,21 +42,114 @@ class BinarySearchTree<T>{
         if(parent.data>data) return this.add_recursive(data,parent.left);
         if(parent.data<data) return this.add_recursive(data,parent.right);
     }
-    public removeElement(data:number,root = this.root){
-        let nodeExists=this.find(data,root);
-        console.log(nodeExists)
-        if(nodeExists!=-1) console.log("node found")
-        else return -1 
+    public remove(data:number,root = this.root){
+        let node=this.find(data,root);
+        if(node!=false&&node!=undefined){
+            console.log(pass("Node",node.data,"found"));
+            this.size--;
+            let parent=node.parent
+            //leef node case
+            if(node.left==null&&node.right==null){
+                //leef node in right
+                if(parent.data<node.data){
+                    parent.right=null;
+                    node.parent=null;
+                    console.log("Removed:",node.data)
+                    return true
+                }
+                //leef node in left
+                if(parent.data>node.data){
+                    parent.left=null;
+                    node.parent=null;
+                    console.log("Removed:",node.data)
+                    return true
+                }
+            }
+            //one child case
+            if(node.left==null&&node.right!==null||node.left!=null&&node.right==null){
+                
+                if(node.left==null&&node.right!==null){
+                    //one child right
+                    let child=node.right;
+                    if(parent.data>node.data){
+                        //node a esquerda
+                        parent.left=child;
+                        child.parent=parent;
+                        node.parent=null;
+                        node.right=null;
+                        console.log("Removido",node.data)
+                        return true
+                    }                    
+                    if(parent.data<node.data){
+                        //node a direita
+                        parent.right=child;
+                        child.parent=parent;
+                        node.parent=null;
+                        node.right=null;
+                        console.log("Removido",node.data)
+                        return true
+                    }                    
+
+                }
+                if(node.left!=null&&node.right==null){
+                    //one child left
+                    let child=node.left;
+                    if(parent.data>node.data){
+                        //node a esquerda
+                        parent.left=child;
+                        child.parent=parent;
+                        node.parent=null;
+                        node.left=null;
+                        console.log("Removido",node.data)
+                        return true
+                    }                    
+                    if(parent.data<node.data){
+                        //node a direita
+                        parent.right=child;
+                        child.parent=parent;
+                        node.parent=null;
+                        node.left=null;
+                        console.log("Removido",node.data)
+                        return true
+                    }  
+                }
+            
+            }
+            //two child case
+            if(node.left!=null&&node.right!=null){
+                console.log("Two Child case")
+                let olddata=node.data;
+                let right=node.right;
+                //find min node
+                let min_node=this.findMinInRightTree(right);
+                node.data=min_node.data;
+                //removing min_node
+                let min_node_parent=min_node.parent;
+                min_node_parent.left=null
+                console.log("Removed:",olddata,"NewNode:",min_node.data)
+                return true
+            }
+
+        }
+        else{
+            console.log(error("Node not found"))
+            return false
+        }
+    }
+    private findMinInRightTree(node:any):any{
+        if(node.left!=null) return this.findMinInRightTree(node.left)
+        else return node;
     }
     public find(data:number,root = this.root):any{
         if(root!=null){
-            if(data==root.data) return root
+            if(data==root?.data) return root
             else {
-                if(root.left==null&&root.right==null) return -1
+                if(root.left==null&&root.right==null) return false
                 if(data<root.data) return this.find(data,root.left);
                 if(data>root.data) return this.find(data,root.right); 
             }
         }
+        else false
     }
 }
 export function TestBinarySearchTree(){
@@ -67,23 +160,29 @@ export function TestBinarySearchTree(){
     console.log(pass("Creating Binary Search Tree[OK]"));
     console.log(warn("---------------------------------------------------"))
     console.log(doing("Adding elements to BST..."));
-        let cached_numbers=[];
+    let cached_numbers=[74,26,100,20,50,90,200,95,199,300,4,180,185,60,40,30,45];
+    cached_numbers.forEach(number=>{
+        bst.addElement(number);
+    })
+    /*  
+    let cached_numbers=[];
         for(let c=10;c>0;c--){
             let number=Math.round(Math.random()*300)
             cached_numbers.push(number);
             bst.addElement(number);    
         }
+    */
     console.log(pass("Adding elements to BST[OK]"));
     console.log(warn("---------------------------------------------------"))
-    console.log(cached_numbers)
     console.log(warn("PRINT ELEMENTS FROM BST..."));
-        cached_numbers.forEach(number => {
-            console.log(bst.find(number));
-        });
+    cached_numbers.forEach(number => {
+            let node=bst.find(number);
+            console.log("Node:",node?.data,"Parent:",node?.parent?.data,"Left:",node?.left?.data,"Right:",node?.right?.data)
+        }
+    );
     console.log(pass("PRINT ELEMENTS FROM BST[OK]"));
-
+    bst.remove(cached_numbers[Math.round(Math.random()*(cached_numbers.length-1))])
 }
-TestBinarySearchTree()
 
 
 
